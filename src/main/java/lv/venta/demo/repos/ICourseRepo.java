253d43cbe43deeae92ad55a743f6eb2dddfaa993
Karina Skirmante <karina.krinkele@venta.lv>
2022-05-12 10:17:39 +0300

    Add: all repos

---
+++ src/main/java/lv/venta/demo/repos/ICourseRepo.java
@@ -0,0 +1,9 @@
+package lv.venta.demo.repos;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lv.venta.demo.models.Course;
+
+public interface ICourseRepo extends CrudRepository<Course, Integer>{
+
+}
